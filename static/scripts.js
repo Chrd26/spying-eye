15dc0c389e0fb02dbe202087c7647693294d370b
Chrd26 <christosdouris@outlook.com>
2023-07-20 21:38:00 +0300

    Added comments

--- static/scripts.js
+++ static/scripts.js
@@ -9,6 +9,8 @@ window.onload = function()
 // Run when the proceedButton is clicked
 proceeedButton.onclick = function()
 {
+  // Add animations and declare variables
+  // To prevent the animation from reseting, use the forwards play mode. source: https://stackoverflow.com/questions/17296919/how-to-prevent-css3-animation-reset-when-finished
   document.getElementById("desc-1").style.animation = "movedesc1 0.5s ease forwards"
   document.getElementById("desc-2").style.animation = "movedesc2 0.5s ease forwards"
   document.getElementById("proceed").style.animation = "moveButton 0.5s ease forwards"
@@ -19,6 +21,7 @@ proceeedButton.onclick = function()
   // Camera Source https://www.kirupa.com/html5/accessing_your_webcam_in_html5.hthttps://www.kirupa.com/html5/accessing_your_webcam_in_html5.htmm
   // Get camera Source width and height https://stackoverflow.com/questions/47593336/how-can-i-detect-width-and-height-of-the-webcamera
   // Now adapt video container to camera size https://www.w3schools.com/js/js_htmldom_css.asp
+  // Run when animation ends Source: https://developer.mozilla.org/en-US/docs/Web/API/Element/animationend_event
   animated.addEventListener("animationend", function(){
     if (!isButtonPressed)
     {
