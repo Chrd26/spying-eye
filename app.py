67be42a26263fdd595dea5dc212069748588122f
Chrd26 <christosdouris@outlook.com>
2023-07-22 21:36:58 +0300

    Cleaned the project a little bit, I had issues with sending video signal to the backend, I found a library that makes connections between WebRTC JavaScript and Python server possible.

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 """Import flask."""
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,send_from_directory
+from flask_socketio import SocketIO, emit
 from ultralytics import YOLO
 import cv2
 import numpy
 
 app = Flask(__name__)
+app.secret_key = "videosstreamapp"
+socketio = SocketIO(app,cors_allowed_origins="*")
 
 @app.route("/", methods = ["POST", "GET"])
 def index():
@@ -12,6 +15,7 @@ def index():
     return render_template("index.html") 
 
 # Create a new route that should run the analysis of the video stream
+# Use webrtc, source: https://www.youtube.com/watch?v=VbbDzx3jCoE
 @app.route("/run_analysis")
 def run_analysis():
     """Analyze the video stream."""
--- static/scripts.js
+++ static/scripts.js
@@ -1,11 +1,33 @@
 const proceeedButton = document.getElementById("proceed");
 const videoStream = document.getElementById("cameraVideo");
 
+
+// Try to get stream and display it on the page.
+// Source: https://webrtc.org/getting-started/media-devices#using-promises
+// Get camera Source width and height https://stackoverflow.com/questions/47593336/how-can-i-detect-width-and-height-of-the-webcamera
+async function playStream()
+{
+  try{
+    const constraints = {"video": true, "audio": false};
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    const videoElement = document.querySelector("video#cameraVideo");
+    const trackSettings = stream.getTracks()[0].getSettings();
+    videoStream.style.display = "block";
+    videoStream.style.width = trackSettings["width"] + "px";
+    videoStream.style.height = trackSettings["height"] + "px";
+    videoElement.srcObject = stream;
+
+    const connection = 
+  }
+  catch(error){
+    console.error("Error opening camera", error);
+  }
+}
+
 window.onload = function()
 {
   console.log("Hello World!");
 }
-
 // Run when the proceedButton is clicked
 proceeedButton.onclick = function()
 {
@@ -19,7 +41,6 @@ proceeedButton.onclick = function()
   let isButtonPressed = true;
 
   // Camera Source https://www.kirupa.com/html5/accessing_your_webcam_in_html5.hthttps://www.kirupa.com/html5/accessing_your_webcam_in_html5.htmm
-  // Get camera Source width and height https://stackoverflow.com/questions/47593336/how-can-i-detect-width-and-height-of-the-webcamera
   // Now adapt video container to camera size https://www.w3schools.com/js/js_htmldom_css.asp
   // Run when animation ends Source: https://developer.mozilla.org/en-US/docs/Web/API/Element/animationend_event
   animated.addEventListener("animationend", function(){
@@ -28,38 +49,18 @@ proceeedButton.onclick = function()
       return;
     }
 
-    if (navigator.mediaDevices.getUserMedia)
-    {
-      isButtonPressed = false;
-      navigator.mediaDevices.getUserMedia({video: true}).then(function(stream)
-        {
-          // Set source to stream
-          videoStream.srcObject = stream;
-          // Get stream settings
-          let getSettings = stream.getTracks()[0].getSettings();
-          console.log(getSettings["width"]);
-          let getWidth = getSettings["width"];
-          let getHeight = getSettings["height"];
+    // Hide uneeded elements
+    document.getElementById("desc-1").style.display="none";
+    document.getElementById("desc-2").style.display="none";
+    document.getElementById("proceed").style.display="none";
+    
+    // Get the back element and display it
+    const backButton = document.getElementById("back");
+    backButton.style.display="block";
 
-           // Apply  webcam width and height to the page
-          // console.log("Width: " + getWidth + " Heignt: " + getHeight + ".")
-          videoStream.style.display = "block";
-          videoStream.style.width = getWidth + "px";
-          videoStream.style.height = getHeight + "px";
+    isButtonPressed = false;
 
-          // Hide uneeded elements
-          document.getElementById("desc-1").style.display="none";
-          document.getElementById("desc-2").style.display="none";
-          document.getElementById("proceed").style.display="none";
-
-          // Get the back element and display it
-          const backButton = document.getElementById("back");
-          backButton.style.display="block";
-        })
-      .catch(function(err0r)
-        {
-          console.log("Something Went Wrong!")
-        });
-    }
+    playStream();
+    
   })
 }
