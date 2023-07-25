ad0da84f160535c09c542fb46a7b20648f62f46e
Chrd26 <christosdouris@outlook.com>
2023-07-25 23:12:38 +0300

    Had trouble with camera, I worked on the working server version and stripped out all its features. Applying it with flask should be ok now

--- Semantics/Working Client to Server and back to Client stream/client.js
+++ Semantics/Working Client to Server and back to Client stream/client.js
@@ -1,14 +1,3 @@
-const proceeedButton = document.getElementById("proceed");
-const videoStream = document.getElementById("cameraVideo");
-
-// Set Up peer connection
-// Source for webrtc connection to python wtih aiortc: https://github.com/aiortc/aiortc/blob/main/examples/server/client.js
-// Try to get stream and display it on the page.
-// Source: https://webrtc.org/getting-started/media-devices#using-promises
-// Get camera Source width and height https://stackoverflow.com/questions/47593336/how-can-i-detect-width-and-height-of-the-webcamera
-
-// Data channel, data channel internal
-let dataChannel = null
 // peer connection
 var pc = null;
 
@@ -20,8 +9,16 @@ function createPeerConnection() {
         sdpSemantics: 'unified-plan'
     };
 
-    config.iceServers = [{urls: ['stun:stun.l.google.com:19302']}];
-
+    config.iceServers = [{
+        urls: "stun:stun.relay.metered.ca:80",
+      },
+      {
+        urls: "turn:a.relay.metered.ca:80",
+        username: "7b2b7284aa3b67f5dbcb3a75",
+        credential: "TdIzFE2tx8kUGA13",
+      }
+    ]
+    
     pc = new RTCPeerConnection(config);
 
     // register some listeners to help debugging
@@ -70,22 +67,11 @@ function negotiate() {
         var offer = pc.localDescription;
         var codec;
 
-        codec = document.getElementById('audio-codec').value;
-        if (codec !== 'default') {
-            offer.sdp = sdpFilterCodec('audio', codec, offer.sdp);
-        }
-
-        codec = document.getElementById('video-codec').value;
-        if (codec !== 'default') {
-            offer.sdp = sdpFilterCodec('video', codec, offer.sdp);
-        }
-
-        document.getElementById('offer-sdp').textContent = offer.sdp;
         return fetch('/offer', {
             body: JSON.stringify({
                 sdp: offer.sdp,
                 type: offer.type,
-                video_transform: document.getElementById('video-transform').value
+                video_transform:"none" 
             }),
             headers: {
                 'Content-Type': 'application/json'
@@ -95,7 +81,6 @@ function negotiate() {
     }).then(function(response) {
         return response.json();
     }).then(function(answer) {
-        document.getElementById('answer-sdp').textContent = answer.sdp;
         return pc.setRemoteDescription(answer);
     }).catch(function(e) {
         alert(e);
@@ -103,6 +88,7 @@ function negotiate() {
 }
 
 function start() {
+    document.getElementById('start').style.display = 'none';
 
     pc = createPeerConnection();
 
@@ -117,46 +103,27 @@ function start() {
         }
     }
 
-      var parameters = {ordered: true};
-
-      dc = pc.createDataChannel('chat', parameters);
-      dc.onclose = function() {
-          clearInterval(dcInterval);
-      };
-      dc.onopen = function() {
-          dcInterval = setInterval(function() {
-              var message = 'ping ' + current_stamp();
-              dc.send(message);
-          }, 1000);
-      };
-      dc.onmessage = function(evt) {
-
-          if (evt.data.substring(0, 4) === 'pong') {
-              var elapsed_ms = current_stamp() - parseInt(evt.data.substring(5), 10);
-          }
-      };
-
     var constraints = {
         audio: false,
         video: true
     };
-        navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-          stream.getTracks().forEach(function(track) {
-            const videoElement = document.querySelector("video#cameraVideo");
-            const trackSettings = stream.getTracks()[0].getSettings();
-            videoStream.style.display = "block";
-            videoStream.style.width = trackSettings["width"] + "px";
-            videoStream.style.height = trackSettings["height"] + "px";
-            pc.addTrack(track, stream);
 
-            return negotiate();
-          });
-        }, function(err) {
-            alert('Could not acquire media: ' + err);
+    document.getElementById('media').style.display = 'block';
+    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+        stream.getTracks().forEach(function(track) {
+            pc.addTrack(track, stream);
         });
-    }
+        return negotiate();
+    }, function(err) {
+        alert('Could not acquire media: ' + err);
+    });
+
+    document.getElementById('stop').style.display = 'inline-block';
+}
 
 function stop() {
+    document.getElementById('stop').style.display = 'none';
+
     // close data channel
     if (dc) {
         dc.close();
@@ -241,45 +208,4 @@ function sdpFilterCodec(kind, codec, realSdp) {
 
 function escapeRegExp(string) {
     return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
-}
-
-window.onload = function()
-{
-  console.log("Hello World!");
-}
-// Run when the proceedButton is clicked
-proceeedButton.onclick = function()
-{
-  // Add animations and declare variables
-  // To prevent the animation from reseting, use the forwards play mode. source: https://stackoverflow.com/questions/17296919/how-to-prevent-css3-animation-reset-when-finished
-  document.getElementById("desc-1").style.animation = "movedesc1 0.5s ease forwards"
-  document.getElementById("desc-2").style.animation = "movedesc2 0.5s ease forwards"
-  document.getElementById("proceed").style.animation = "moveButton 0.5s ease forwards"
-  let newButton;
-  const animated = document.getElementById("proceed")
-  let isButtonPressed = true;
-
-  // Camera Source https://www.kirupa.com/html5/accessing_your_webcam_in_html5.hthttps://www.kirupa.com/html5/accessing_your_webcam_in_html5.htmm
-  // Now adapt video container to camera size https://www.w3schools.com/js/js_htmldom_css.asp
-  // Run when animation ends Source: https://developer.mozilla.org/en-US/docs/Web/API/Element/animationend_event
-  animated.addEventListener("animationend", function(){
-    if (!isButtonPressed)
-    {
-      return;
-    }
-
-    // Hide uneeded elements
-    document.getElementById("desc-1").style.display="none";
-    document.getElementById("desc-2").style.display="none";
-    document.getElementById("proceed").style.display="none";
-    
-    // Get the back element and display it
-    const backButton = document.getElementById("back");
-    backButton.style.display="block";
-
-    isButtonPressed = false;
-
-    //streamMain();
-    start();
-  })
 }
--- app.py
+++ app.py
@@ -1,54 +1,124 @@
-"""Import All.""" 
-import asyncio
-import secrets
-import uuid
-import asyncio
 
-import aiortc as aio
-from flask import Flask,render_template 
-from flask_cors import CORS
-from ultralytics import YOLO
+import argparse
+import asyncio
+import json
+import logging
+import os
+import ssl
+import uuid
+
 import cv2
-import numpy
-from helpers import VideoManager
+from aiohttp import web
+from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
+from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder, MediaRelay
+from av import VideoFrame
+from flask import Flask, render_template
 
 app = Flask(__name__)
-cors(app)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-
+ROOT = os.path.dirname(__file__)
+logger = logging.getLogger("pc")
 pcs = set()
-dcs = set()
+relay = MediaRelay()
 
-event_loop = asyncio.new_event_loop()
-asyncio.set_event_loop(event_loop)
 
-# On start load index.htnl
-@app.route("/", methods = ["POST", "GET"])
-def index():
-    """Load the Index webpage."""
+class VideoTransformTrack(MediaStreamTrack):
+    """
+    A video stream track that transforms frames from an another track.
+    """
+
+    kind = "video"
+
+    def __init__(self, track, transform):
+        super().__init__()  # don't forget this!
+        self.track = track
+        self.transform = transform
+
+    async def recv(self):
+        frame = await self.track.recv()
+
+        if self.transform == "cartoon":
+            img = frame.to_ndarray(format="bgr24")
+
+            # prepare color
+            img_color = cv2.pyrDown(cv2.pyrDown(img))
+            for _ in range(6):
+                img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
+            img_color = cv2.pyrUp(cv2.pyrUp(img_color))
+
+            # prepare edges
+            img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            img_edges = cv2.adaptiveThreshold(
+                cv2.medianBlur(img_edges, 7),
+                255,
+                cv2.ADAPTIVE_THRESH_MEAN_C,
+                cv2.THRESH_BINARY,
+                9,
+                2,
+            )
+            img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
+
+            # combine color and edges
+            img = cv2.bitwise_and(img_color, img_edges)
+
+            # rebuild a VideoFrame, preserving timing information
+            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+            new_frame.pts = frame.pts
+            new_frame.time_base = frame.time_base
+            return new_frame
+        elif self.transform == "edges":
+            # perform edge detection
+            img = frame.to_ndarray(format="bgr24")
+            img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+
+            # rebuild a VideoFrame, preserving timing information
+            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+            new_frame.pts = frame.pts
+            new_frame.time_base = frame.time_base
+            return new_frame
+        elif self.transform == "rotate":
+            # rotate image
+            img = frame.to_ndarray(format="bgr24")
+            rows, cols, _ = img.shape
+            M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
+            img = cv2.warpAffine(img, M, (cols, rows))
+
+            # rebuild a VideoFrame, preserving timing information
+            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+            new_frame.pts = frame.pts
+            new_frame.time_base = frame.time_base
+            return new_frame
+        else:
+            return frame
+
+@app.route("/", methods=["GET"])
+async def index():
     return render_template("index.html")
 
-# Offer
-@app.route("/offer", methods=["POST"])
-def offer():
-    params = request.json
-    setOffer = aio.RTCSessionDescription(sdp=params["sdp"], type=params["type"])
 
-    pc = aio.RTCPeerConnection()
-    pc_id = "PeerConnections(%s)" % uuid.uuid4()
-    pcs.add(pc)
+async def javascript(request):
+    content = open(os.path.join(ROOT, "client.js"), "r").read()
+    return web.Response(content_type="application/javascript", text=content)
 
-
-# Use webrtc, source: https://www.youtube.com/watch?v=VbbDzx3jCoE
-@app.route("/run_analysis")
-# Handle Upcoming traffic source: https://github.com/aiortc/aiortc/blob/main/examples/server/server.py
+@app.route("/offer", methods=["GET", "POST"])
 async def offer(request):
-    """Handle Incoming data."""
     params = await request.json()
-    offer = RTCSessionDescription(sdp = params["sdp"], type=params["type"])
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
 
     pc = RTCPeerConnection()
+    pc_id = "PeerConnection(%s)" % uuid.uuid4()
     pcs.add(pc)
+
+    def log_info(msg, *args):
+        logger.info(pc_id + " " + msg, *args)
+
+    log_info("Created for %s", request.remote)
+
+    # prepare local media
+    player = MediaPlayer(os.path.join(ROOT, "demo-instruct.wav"))
+    if args.record_to:
+        recorder = MediaRecorder(args.record_to)
+    else:
+        recorder = MediaBlackhole()
 
     @pc.on("datachannel")
     def on_datachannel(channel):
@@ -57,26 +127,80 @@ async def offer(request):
             if isinstance(message, str) and message.startswith("ping"):
                 channel.send("pong" + message[4:])
 
-    @pc.on("iceconnectionstatechange")
-    async def on_iceconnectionstatechange():
-        if pc.iceConnectionState == "failed":
+    @pc.on("connectionstatechange")
+    async def on_connectionstatechange():
+        log_info("Connection state is %s", pc.connectionState)
+        if pc.connectionState == "failed":
             await pc.close()
             pcs.discard(pc)
 
-    @pc.on("icegatheringstatechange")
-    async def on_icegatheringstatechange():
-        if pc.iceGatheringState == "complete":
-            ice_candidates = [{"spdMLineIndex": c.sdpMLineIndex, "candidate": c.candidate} for c in pc.localDescription.sdp.splitelines() if c.startswith("a=candidate:")]
-            response = {"sdp": {"type": pc.localDescription.type, "sdp": pc.localDescription.sdp}, "ice_candidates": ice_candidates}
-            return web.Response(content_type = "application.json", text = json.dumps(response))
-
     @pc.on("track")
     def on_track(track):
-        pc.addTrack(RELAY.subscribe(track))
+        log_info("Track %s received", track.kind)
 
+        if track.kind == "audio":
+            pc.addTrack(player.audio)
+            recorder.addTrack(track)
+        elif track.kind == "video":
+            pc.addTrack(
+                VideoTransformTrack(
+                    relay.subscribe(track), transform=params["video_transform"]
+                )
+            )
+            if args.record_to:
+                recorder.addTrack(relay.subscribe(track))
+
+        @track.on("ended")
+        async def on_ended():
+            log_info("Track %s ended", track.kind)
+            await recorder.stop()
+
+    # handle offer
     await pc.setRemoteDescription(offer)
+    await recorder.start()
+
+    # send answer
     answer = await pc.createAnswer()
     await pc.setLocalDescription(answer)
 
-    return web.response(content_type="application/json", text=json.dumps({"sdp":{"type": pc.localDescription.type, "sdp": pc.localDescription.sdp}}))
+    return web.Response(
+        content_type="application/json",
+        text=json.dumps(
+            {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+        ),
+    )
 
+
+async def on_shutdown(app):
+    # close peer connections
+    coros = [pc.close() for pc in pcs]
+    await asyncio.gather(*coros)
+    pcs.clear()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="WebRTC audio / video / data-channels demo"
+    )
+    parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
+    parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
+    parser.add_argument(
+        "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
+    )
+    parser.add_argument(
+        "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
+    )
+    parser.add_argument("--record-to", help="Write received media to a file."),
+    parser.add_argument("--verbose", "-v", action="count")
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    if args.cert_file:
+        ssl_context = ssl.SSLContext()
+        ssl_context.load_cert_chain(args.cert_file, args.key_file)
+    else:
+        ssl_context = None
