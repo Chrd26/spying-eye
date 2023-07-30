9f95e1d0ba0a2f83beabe75957e4a1fe29372c40
Chrd26 <christosdouris@outlook.com>
2023-07-30 23:28:35 +0300

    Set up layout template, set up project for the login thingy

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 """Import Flask."""
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 # app
 app = Flask(__name__)
 
+# Login Page
 @app.route("/", methods=["GET", "Method"])
-def index():
-    """Render Index."""
+def login():
+    """Render Index, Login Page."""
     return render_template("index.html")
 
+# Register User
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register User."""
 
-
-
-
+# Load Index
+@app.route("/index", methods=["GET", "POST"])
+def index():
+    """Load Index."""
 
 
 if __name__ == "__main__":
