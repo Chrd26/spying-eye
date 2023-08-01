587bf03c1689446fb91cc1e5c5852eb1831d0c23
Chrd26 <christosdouris@outlook.com>
2023-08-01 23:08:08 +0300

    Started working on index page, main page needs a description.

--- static/scripts.js
+++ static/scripts.js
@@ -1,37 +1,44 @@
-const proceeedButton = document.getElementById("proceed");
-const videoStream = document.getElementById("cameraVideo");
+let video;
+let detector;
+let detections = [];
+let startDetectionBtn = document.getElementById("start-detection");
 
-// Start when proceeed is pressed
-proceeedButton.onclick = function()
-{
-  // Add animations and declare variables
-  // To prevent the animation from reseting, use the forwards play mode. source: https://stackoverflow.com/questions/17296919/how-to-prevent-css3-animation-reset-when-finished
-  document.getElementById("desc-1").style.animation = "movedesc1 0.5s ease forwards"
-  document.getElementById("desc-2").style.animation = "movedesc2 0.5s ease forwards"
-  document.getElementById("proceed").style.animation = "moveButton 0.5s ease forwards"
-  let newButton;
-  const animated = document.getElementById("proceed")
-  let isButtonPressed = true;
 
-  // Camera Source https://www.kirupa.com/html5/accessing_your_webcam_in_html5.hthttps://www.kirupa.com/html5/accessing_your_webcam_in_html5.htmm
-  // Now adapt video container to camera size https://www.w3schools.com/js/js_htmldom_css.asp
-  // Run when animation ends Source: https://developer.mozilla.org/en-US/docs/Web/API/Element/animationend_event
-  animated.addEventListener("animationend", function(){
-    if (!isButtonPressed)
-    {
-      return;
+  console.log("start detection");
+
+  function gotDetections(error, results){
+    if (error){
+      console.log(error);
+    }else{
+      console.log(results);
+      detections = results;
+      detector.detect(video, gotDetections);
     }
+  }
 
-    // Hide uneeded elements
-    document.getElementById("desc-1").style.display="none";
-    document.getElementById("desc-2").style.display="none";
-    document.getElementById("proceed").style.display="none";
-    videoStream.style.display="block";
-    
-    // Get the back element and display it
-    const backButton = document.getElementById("back");
-    backButton.style.display="block";
+  function setup(){
+    detector = ml5.objectDetector("cocossd");
+    video = createCapture(video);
+    video.size(640, 480);
+    detector.detect(video, gotDetections);
+  }
 
-    isButtonPressed = false;
-  })
-}
+  function draw(){
+    background(220);
+    image(video, 0, 0);
+
+    for (let i = 0; i < detections.length; i++){
+      let object = detections[i];
+
+      // Create Bounding Boxes
+      stroke(0, 255, 0);
+      strokeWeight(4);
+      noFill();
+      rect(object.x, object.y, object.width, object.height)
+
+      // Create label for each object detected
+      noStroke();
+      fill(255);
+      text(object.label, object.x + 10, object.y + 24);
+    }
+  }
